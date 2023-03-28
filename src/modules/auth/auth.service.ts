5ec6e1a298741e3c70ed20f15e13989db6e7c6ed
Paul Ajuizeogu <paul.a@mblhightech.com>
2023-03-28 17:50:26 +0300

    Authentication and Authorization setup

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/login-auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
 
   async createUser(values: CreateUserDto) {
+    const salt = await bcrypt.genSalt();
+    values.password = await bcrypt.hash(values.password, salt);
+
     return this.userService.createOne(undefined, values);
   }
 
   async login(values: LoginDto) {
-    return { values, action: 'login' };
+    const user = await this.userService.findOne({
+      where: {
+        email: values.email,
+      },
+    });
+
+    if (user) {
+      const validePassword = await bcrypt.compare(
+        values.password,
+        user.password,
+      );
+
+      if (validePassword) {
+        return 'logged in';
+      }
+
+      throw new UnauthorizedException('Incorrect email or password');
+    }
+
+    throw new NotFoundException('Unauthorized');
   }
 }
